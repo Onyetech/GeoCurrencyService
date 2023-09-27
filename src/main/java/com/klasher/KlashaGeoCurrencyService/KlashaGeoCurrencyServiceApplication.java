44c4c5b97f5947864c6c96f8bb1c301992482f0c
onyetech <ikennajamesokonkwo@gmail.com>
2023-09-27 22:41:33 +0100

    Initial commit

---
+++ src/main/java/com/klasher/KlashaGeoCurrencyService/KlashaGeoCurrencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.klasher.KlashaGeoCurrencyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlashaGeoCurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlashaGeoCurrencyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klasher/KlashaGeoCurrencyService/KlashaGeoCurrencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klasher.KlashaGeoCurrencyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlashaGeoCurrencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
